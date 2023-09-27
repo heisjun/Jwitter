67325873230fdc5fc3b4f60d3d20ec84434570c4
heisjun <jun146517@gmail.com>
2023-09-27 17:58:30 +0900

    Feat : Add Profile Page
    프로필 페이지 생성

---
+++ src/routes/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <div>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </div>
+  );
+};
+
+export default Profile;
